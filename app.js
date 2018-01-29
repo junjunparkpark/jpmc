afceb108cce0730ca2458b412c93ea27b50052eb
Jun Park <jun.park@live.com>
2018-01-29 13:33:14 -0800

    Finish test specs

--- app.js
+++ app.js
@@ -1,78 +1,70 @@
 
 
-class Calculator {
+export class Calculator {
   add() {
     return Array.from(arguments).reduce((acc, cur) => acc + cur);
-  }
+  };
 
   subtract() {
     return Array.from(arguments).reduce((acc, cur) => acc - cur);
-  }
+  };
 
   multiply() {
     return Array.from(arguments).reduce((acc, cur) => acc * cur);
-  }
+  };
 
   divide() {
-    if (arguments[1] === 0) return NaN;
+    let divisor = arguments[1];
+    if (divisor === 0) return NaN;
+
     return Array.from(arguments).reduce((acc, cur) => acc / cur);
-  }
-}
+  };
+};
 
-const extend = (a, b) => {
-  for (var key in a) {
-    b[key] = a[key];
-  }
-}
-
-class ScientificCalculator extends Calculator {
+export class ScientificCalculator extends Calculator {
   sin() {
     return Math.sin(arguments[0]);
-  }
+  };
 
   cos() {
     return Math.cos(arguments[0]);
-  }  
+  }; 
 
   tan() {
     return Math.tan(arguments[0]);
-  }
+  };
 
   log() {
     return Math.log(arguments[0]);
-  }
+  };
 
-}
+};
 
+export function withExponents(superclass) {
+  return Object.assign({}, superclass, {
+    pow(a, b) {
+      return Math.pow(a, b);
+    },
 
-const withExponents = {
-  pow: (a, b) => {
-    return Math.pow(a, b);
-  },
+    multiplyExp(a, b) {
+      return Math.pow(a[0], a[1]) * Math.pow(b[0], b[1]);
+    },
 
-  multiplyExp: (a, b) => {
-    return pow(a) * pow(b);
-  },
+    divideExp(a, b) {
+      let divisor = Math.pow(b[0], b[1]);
+      if (divisor === 0) return NaN;
 
-  divideExp: (a, b) => {
-    return pow(a) / pow(b);
-  }
-}
+      return Math.pow(a[0], a[1]) / divisor;
+    }
+  });
+};
 
-const delay = async (time, fn, method, args) => {
-  return (new Promise((resolve, reject) => {
+export const delay = (time, fn, method, args) => {
+  return new Promise((resolve, reject) => {
     if (!fn[method]) reject(new Error('Function does not exist'));
 
-    
-    let delayedFn = await setTimeout(() => {
-      return fn[method].call(args);
-    }, time)();
-
-    resolve(delayedFn);
-  }))();
-}
-
-module.exports.Calculator = Calculator;
-module.exports.ScientificCalculator = ScientificCalculator;
-module.exports.withExponents = withExponents;
-module.exports.delay = delay;
+    setTimeout(() => {
+      resolve(fn[method].apply(null, args));
+    }, time);
+  });
+};
